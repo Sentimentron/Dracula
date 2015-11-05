4f8f877c505536e441217946745f04d8860b5e29
Richard Townsend <richard@sentimentron.co.uk>
2015-11-05 23:15:03 +0000

    Alright, we have something that is correct

--- nn_layers.py
+++ nn_layers.py
@@ -32,40 +32,28 @@ def lstm_mask_layer(proj, mask):
 
     return proj * mask[:, :, None]
 
-
-def per_word_averaging_layer(proj, wmask, n_samples, dim):
+def per_word_averaging_layer(proj, wmask, n_chars, n_samples, n_proj):
     """
-    Takes a word-mask and a masked LSTM output, produces a per-word summary.
-    :param proj: LSTM output
-    :param wmask: word indices matrix
-    :param n_samples: maximum number of samples
-    :param dim: size of word-embeddings
-    :return: per-word average of all character embeddings.
+    :param proj: Output of the LSTM layer
+    :param wmask: Unravelled 4D-index tensor (represented in 2d)
+    :param n_chars: The maximum number of characters (e.g. 140)
+    :param n_samples: The number of things in this mini-batch
+    :param dim: The number of dimensions coming out the LSTM
+    :return: The per-word averages.
     """
-    avg_layer = tensor.alloc(numpy_floatX(0.), 16, n_samples, dim)
-    count_layer = tensor.alloc(0, 16, n_samples, dim)
-    fixed_ones = tensor.ones_like(count_layer)
-
-    def set_value_at_position(location, output_model, count_model, fixed_ones, values):
-        print location.type, values.type, output_model.type
-        output_subtensor = output_model[location[0], location[1]]
-        count_subtensor = count_model[location[0], location[1]]
-        ones_subtensor = fixed_ones[location[0], location[1]]
-        values_subtensor = values[location[2], location[1]]
-        return tensor.inc_subtensor(output_subtensor, values_subtensor), \
-               tensor.inc_subtensor(count_subtensor, ones_subtensor)
-
-    (avg_layer, count_layer), _ = theano.foldl(fn=set_value_at_position,
-                                               sequences=[wmask],
-                                               outputs_info=[avg_layer, count_layer],
-                                               non_sequences=[fixed_ones, proj]
-                                               )
-
-    count_layer_inverse = 1.0 / count_layer
-    count_layer_mask = 1.0 - tensor.isinf(count_layer_inverse)
-    count_layer_mult = tensor.isinf(count_layer_inverse) + count_layer
-    return (avg_layer / count_layer_mult) * count_layer_mask
-
+    tmp = tensor.alloc(0.0, n_chars * n_samples * 16, n_proj)
+    #scattered2 = theano.tensor.inc_subtensor(tmp[wmask.flatten()].dimshuffle('x', 1), theano.tensor.reshape(proj, (n_chars * n_samples, dim)))
+    #scattered2 = tmp[wmask.flatten()] + theano.tensor.reshape(proj, (n_chars * n_samples, dim))
+    #rscattered = theano.tensor.reshape(scattered2, (n_chars, n_samples, 16, dim))
+    tmp = theano.tensor.inc_subtensor(tmp[theano.tensor.flatten(wmask)], theano.tensor.reshape(proj, (n_chars * n_samples, n_proj)))
+    #tmp = numpy.reshape(tmp, (n_chars, n_samples, 16, n_proj))
+    #tmp = numpy.reshape(tmp, (n_chars, n_samples, 16, n_proj))
+    tmp = theano.tensor.reshape(tmp, (n_chars, n_samples, 16, n_proj))
+    #tmp = 
+    divider = theano.tensor.neq(tmp, 0.0).sum(axis=0)
+    divider += theano.tensor.eq(divider, 0.0) # Filter NaNs
+    return tmp.sum(axis=0) / divider
+    #return tmp
 
 def softmax_layer(dropout_mask, avg_per_word, U, b, y_mask):
     """
