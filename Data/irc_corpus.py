c13fcd77513db9cb51e915ee6d328d37789f5941
Richard Townsend <richard@sentimentron.co.uk>
2016-04-11 22:57:45 +0100

    Converting the IRC corpus

--- Data/irc_corpus.py
+++ Data/irc_corpus.py
@@ -6,5 +6,19 @@ if __name__ == "__main__":
 
     for sentence in nps_chat.tagged_posts():
         for word, pos in sentence:
+            if pos == '^PRP^VBP':
+                pos = 'PRP$'
+            if '^' in pos:
+                pos = pos.replace('^', '')
+            if pos == 'BES':
+                pos = 'VBZ'
+            if pos == 'HVS':
+                pos = 'VBZ'
+            if pos == 'X':
+                pos = 'UH'
+            if pos == 'GW':
+                pos = 'PRP'
+            if pos == 'LS':
+                pos = 'JJS'
             print '{}\t{}'.format(word, pos)
         print
